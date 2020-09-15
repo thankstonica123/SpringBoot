1ff5194d337a58bbe8fa0aec3b803d1dc8e9fd1a
thankstonica123 <thankstonica@126.com>
2020-09-15 19:10:36 +0800

    add a module to git

---
+++ spring_01_testSpring/src/main/java/com/thankstinoca/spring/GetBeans.java
@@ -0,0 +1,10 @@
+package com.thankstinoca.spring;
+
+/**
+ * Created by thankstonica on 2020/9/15.
+ */
+public class GetBeans {
+    public static void main(String[] args) {
+        System.out.println("hello spring");
+    }
+}
